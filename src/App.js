0405f437a86c9770902a75ff1308c32e096cf412
Pedro <pedromedina190@gmail.com>
2019-02-27 04:31:27 -0300

    important changes like:  setting a defalut message for ingredients and dinamyc ingredients

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layouts/Layout';
+import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
+      </div>
     );
   }
 }
