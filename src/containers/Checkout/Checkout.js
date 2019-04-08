3873c23481e600829818b3b3c76715dfa6134393
Pedro <pedromedina190@gmail.com>
2019-04-08 05:48:42 -0400

    Order submission handling and passing data trough pages

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,19 +5,21 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state= {
-        ingredients:{
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients:null,
+        price: 0
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1]
+            if(param[0] === 'price'){
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1]
+            }
+            
         }
         this.setState({ ingredients:ingredients });
     }
@@ -38,7 +40,9 @@ class Checkout extends Component {
                     checkoutContinue={this.checkoutContinueHandler}
                     checkoutCancel={this.checkoutCancelHandler}
                 />
-                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+                <Route 
+                    path={this.props.match.path + '/contact-data'} 
+                    render={ (props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>) } />
             </div>
         );
     }
