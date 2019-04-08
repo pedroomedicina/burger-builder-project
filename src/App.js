c951b90518af5fdb9ac38c0ec07a60e0d8485047
Pedro <pedromedina190@gmail.com>
2019-04-08 02:45:51 -0400

    enabled routing and added some routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Layout from './components/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -9,8 +11,10 @@ class App extends Component {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path='/' exact component={BurgerBuilder}/>
+            <Route path='/checkout' component={Checkout}/>
+          </Switch>
         </Layout>
       </div>
     );
