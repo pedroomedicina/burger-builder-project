2af13aa0ffc59a0f28a8e983fad257b1f7c96ee8
Pedro <pedromedina190@gmail.com>
2019-04-08 04:51:00 -0400

    added a contact data form, nested into checkout page and styled.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state= {
@@ -36,6 +38,7 @@ class Checkout extends Component {
                     checkoutContinue={this.checkoutContinueHandler}
                     checkoutCancel={this.checkoutCancelHandler}
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         );
     }
