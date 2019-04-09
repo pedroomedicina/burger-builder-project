1ea0e3e18e2b37bb356555f3fccc33ba8ca28ec0
Pedro <pedromedina190@gmail.com>
2019-04-09 05:57:49 -0400

    set a Js config object for the contact data form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import classes from './Input.css';
 
-const input = ( props ) =>{
+const input = (props) => {
     let inputElement = null;
 
-    switch(props.inputtype){
-        case('input'):
-            inputElement = <input className={classes.InputElement}  {...props} />;
+    switch (props.elementtype) {
+        case ('input'):
+            inputElement = <input className={classes.InputElement}  {...props.elementconfig} value={props.value} />;
             break;
-        case('textarea'):
-            inputElement = <textarea className={classes.InputElement}  {...props} />;
+        case ('textarea'):
+            inputElement = <textarea className={classes.InputElement}  {...props.elementconfig} value={props.value} />;
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props} />;
+            inputElement = <input className={classes.InputElement} {...props.elementconfig} value={props.value}/>;
     }
 
-    return(
+    return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
         </div>
     );
-} 
+}
 
 export default input;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -7,60 +9,129 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementtype: 'input',
+                elementconfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementtype: 'input',
+                elementconfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementtype: 'input',
+                elementconfig: {
+                    type: 'text',
+                    placeholder: 'ZIP CODE'
+                },
+                value: ''
+            },
+            country: {
+                elementtype: 'input',
+                elementconfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementtype: 'input',
+                elementconfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementtype: 'select',
+                elementconfig: {
+                    options: [{
+                            value: 'fastest',
+                            displayValue: 'fastest'
+                        },
+                        {
+                            value: 'cheapest',
+                            displayValue: 'cheapest'
+                        }
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
-    
+
     orderHandler = (e) => {
         e.preventDefault();
-        this.setState({ loading: true });
+        this.setState({
+            loading: true
+        });
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price, //this sould be calculated in the server!
-            //some dummy data...
-            customer: {
-                name: 'Pedro Medina',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Chile'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)
-                .then(response => {
-                    this.setState({ loading: false });
-                    this.props.history.push('/');
-                }).catch(error => this.setState({ loading: false }));
+            .then(response => {
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/');
+            }).catch(error => this.setState({
+                loading: false
+            }));
     }
 
-    render () {
-        let form = (
-            <form>
-                    <Input inputtype='input' type='text' name='name' placeholder='Your Name'/>
-                    <Input inputtype='input' type='text' name='email' placeholder='Your Email'/>
-                    <Input inputtype='input' type='text' name='street' placeholder='Street'/>
-                    <Input inputtype='input' type='text' name='postal' placeholder='Postal Code'/>
-                    <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
-            </form>
+    render() {
+        let form = ( <
+            form >
+            <
+            Input elementtype = ''
+            elementconfig = ''
+            value = '' / >
+            <
+            Input inputtype = 'input'
+            type = 'text'
+            name = 'email'
+            placeholder = 'Your Email' / >
+            <
+            Input inputtype = 'input'
+            type = 'text'
+            name = 'street'
+            placeholder = 'Street' / >
+            <
+            Input inputtype = 'input'
+            type = 'text'
+            name = 'postal'
+            placeholder = 'Postal Code' / >
+            <
+            Button btnType = 'Success'
+            clicked = {
+                this.orderHandler
+            } > ORDER < /Button> < /
+            form >
         );
-        if (this.state.loading){
-            form = <Spinner />;
-        } 
-        return (
-            <div className={classes.ContactData}>
-                <h4>Enter your Contact Data</h4>
-                {form}
-            </div>
-        )
-    }
+        if (this.state.loading) {
+            form = < Spinner / > ;
+        }
+        return ( <
+                div className = {
+                    classes.ContactData
+                } >
+                <
+                h4 > Enter your Contact Data < /h4> {
+                form
+            } <
+            /div>
+    )
+}
 }
 
 export default ContactData;
