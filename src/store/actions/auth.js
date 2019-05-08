9e734dece6bd20db07a287cfbac3d59a669adef4
pedroomedicina <pedromedina190@gmail.com>
2019-05-07 23:19:17 -0400

    Agregadas funcionalidades de crear cuenta y hacer login (obtener token acceso), Agregado un switch para cambiar de modo sign up a modo login y viceversa

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -20,8 +21,27 @@ export const authFail = (error) => {
     };
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAZo2h_b96SOiTEUAEkflow9sW1I22wTFA";
+        if (!isSignUp){
+            url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAZo2h_b96SOiTEUAEkflow9sW1I22wTFA";
+
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 }
