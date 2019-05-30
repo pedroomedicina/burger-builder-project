a51da6ba9575960a040c297a26ea879978eba190
pedroomedicina <pedromedina190@gmail.com>
2019-05-30 14:27:32 -0400

    changes until handling automatic authentication with saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -39,3 +39,20 @@ export function* authUserSaga(action) {
             yield put(error.authFail(error.response.data.error));
         }
 }
+
+export function* authCheckStateSaga(action) {
+    const token = yield localStorage.getItem('token');
+        if (!token){
+            yield put(actions.logout());
+        } else {
+            const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
+            if ( expirationDate <= new Date()){
+                yield put(actions.logout());
+            } else {
+                const userId = yield localStorage.getItem('userId');
+                yield put(actions.authSuccess(token, userId));
+                yield put(actions.checkAuthTimeout(
+                    (expirationDate.getTime() - new Date().getTime()) / 1000));
+            }
+        }
+}
