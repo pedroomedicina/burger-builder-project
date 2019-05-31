2f662acf50b027906d23b43b4a20b6ff760fc69c
pedroomedicina <pedromedina190@gmail.com>
2019-05-30 22:10:40 -0400

    last commit into redux-saga introduction

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,6 +2,7 @@ import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
 import { iniIngredientsSaga } from './burgerBuilder';
+import { purchaseBugerSaga, fetchOrdersSaga } from './order';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
@@ -12,4 +13,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, iniIngredientsSaga)
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBugerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
